6aa6731dd426df46e863d28297f25599a0c21274
Ammar Zarzour <azar41550@gmail.com>
2024-12-02 12:44:00 +0300

    add crud functionality

--- client.py
+++ client.py
@@ -5,53 +5,138 @@ HOST = "127.0.0.1"
 PORT = 3000
 
 
-def get_action() -> int:
-    try:
-        action = int(input(
-            """
-    enter the action you want to do\n
-    1. login
-    2. register
-    """))
-        if action < 0 or action > 2:
-            raise ValueError
-
-        return action
-
-    except:
-        print("please enter a valid choice")
+def get_valid_number(message: str):
+    while True:
+        try:
+            amount = float(input(message))
+            return amount
+        except ValueError:
+            print("Enter a valid number")
 
 
-def do_login(conn):
-    pass
-    # name = input("user name: ")
-    # password = input("password: ")
-    # conn.sendall(json.dumps(
-    #     {'user_name': name, 'password': password, "action": 1}).encode()
-    # )
-    # data = conn.recv(2048)
-    # print(data)
+class Client:
+    ISLOGED_IN = False
+
+    @staticmethod
+    def get_auth_actions() -> int:
+        print(Client.ISLOGED_IN)
+        try:
+            action = int(input(
+                """
+enter the action you want to do
+1. login
+2. register
+"""))
+            if action < 0 or action > 2:
+                raise ValueError
+
+            return action
+
+        except:
+            print("please enter a valid choice")
+
+    @staticmethod
+    def get_actions() -> int:
+        print(Client.ISLOGED_IN)
+        try:
+            action = int(input(
+                """
+enter the action you want to do
+1. deposit
+2. withdraw
+3. show balance
+"""))
+            if action < 0 or action > 3:
+                raise ValueError
+
+            return action + 2
+
+        except:
+            print("please enter a valid choice")
+
+    @staticmethod
+    def get_response(conn: socket.socket):
+        data = conn.recv(2048)
+        print(data)
+        return json.loads(data)
+
+    @staticmethod
+    def login(conn: socket.socket):
+        name = input("user name: ")
+        password = input("password: ")
+        conn.sendall(json.dumps(
+            {'user_name': name, 'password': password, "action": 1}).encode()
+        )
+        data = Client.get_response(conn)
+        if (data.get("status") == "success"):
+            Client.ISLOGED_IN = True
+
+    @staticmethod
+    def register(conn: socket.socket):
+        name = input("user name: ")
+        password = input("password: ")
+        conn.sendall(json.dumps(
+            {'user_name': name, 'password': password, "action": 2}).encode()
+        )
+        Client.get_response(conn)
+
+    @staticmethod
+    def deposit(conn: socket.socket):
+        amount = get_valid_number("enter the amount you want to deposit: ")
+        # TODO: make user id dynamic
+        user_id = 1
+        conn.sendall(json.dumps(
+            {'user_id': user_id, 'amount': amount, "action": 3}).encode()
+        )
+        Client.get_response(conn)
+
+    @staticmethod
+    def withdraw(conn: socket.socket):
+        amount = get_valid_number("enter the amount you want to withdraw: ")
+        user_id = 1
+        conn.sendall(json.dumps(
+            {'user_id': user_id, 'amount': amount, "action": 4}).encode()
+        )
+        Client.get_response(conn)
 
 
-def do_register(conn):
-    name = input("user name: ")
-    password = input("password: ")
-    conn.sendall(json.dumps(
-        {'user_name': name, 'password': password, "action": 2}).encode()
-    )
-    data = conn.recv(2048)
-    print(data)
+    @staticmethod
+    def show_balance(conn: socket.socket):
+        user_id = 1
+        conn.sendall(json.dumps(
+            {'user_id': user_id, "action": 5}).encode()
+        )
+        Client.get_response(conn)
+
+    @staticmethod
+    def perform_action(conn, action):
+        if action == 1:
+            Client.login(conn)
+        if action == 2:
+            Client.register(conn)
+
+        if action > 2 and not Client.ISLOGED_IN:
+            print("Method not allowd")
+            return
+
+        if action == 3:
+            Client.deposit(conn)
+        if action == 4:
+            Client.withdraw(conn)
+        if action == 5:
+            Client.show_balance(conn)
+
+    @staticmethod
+    def start():
+        while True:
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as connection:
+                connection.connect((HOST, PORT))
+                if Client.ISLOGED_IN:
+                    action = Client.get_actions()
+                else:
+                    action = Client.get_auth_actions()
+                Client.perform_action(connection, action)
 
 
-def do_action(conn, action):
-    if action == 1:
-        do_login(conn)
-    if action == 2:
-        do_register(conn)
+Client.start()
 
-
-while True:
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as connection:
-        connection.connect((HOST, PORT))
-        action = get_action()
-        do_action(connection, action)
--- models.py
+++ models.py
@@ -1,6 +1,7 @@
-import hashlib
-from sqlalchemy import Column, Integer, String, Double
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
+import hashlib
+import json
 
 from database import DataBaseHandeler
 
@@ -12,38 +13,96 @@ class User(Base):
     user_id = Column(Integer, primary_key=True)
     user_name = Column(String, nullable=False, unique=True)
     password = Column(String, nullable=False)
-    balance = Column(Double, nullable=False)
+    balance = Column(String, nullable=False)
 
     @staticmethod
-    def _check_user_name_uniquenes(session, user_name):
+    def _check_user_name_uniquenes(session, user_name) -> None:
         user = session.query(User).filter_by(user_name=user_name).first()
         if (user):
             raise ValueError
 
     @staticmethod
-    def create_user(user: "User"):
+    def _hash_password(password) -> str:
         salt = "5gz"
-        salted_password = user.password + salt
+        salted_password = password + salt
         hashed = hashlib.md5(salted_password.encode())
-        user.password = hashed.hexdigest()
+        return hashed.hexdigest()
+
+    @staticmethod
+    def create_user(user: "User"):
+        user.password = User._hash_password(password=user.password)
         session = DataBaseHandeler.get_session()
+
         try:
             User._check_user_name_uniquenes(
                 session=session, user_name=user.user_name
             )
         except ValueError:
-            return "User alread exists"
+            return json.dumps(
+                {'status': "error", 'message': "Username has already been taken"}
+            )
 
         session.add(user)
         session.commit()
-
-        return "user created sucsessfully"
+        return json.dumps(
+            {'status': "success", 'message': "User created successfully"}
+        )
 
     @staticmethod
-    def get_user(user_name: str, password: str) -> "User":
+    def login(user_name: str, password: str):
         session = DataBaseHandeler.get_session()
         user = session.query(User).filter_by(user_name=user_name).first()
-        return user
+
+        if (not user or User._hash_password(password) != user.password):
+            return json.dumps(
+                {'status': "error", 'message': "User does not exists"}
+            )
+
+        return json.dumps(
+            {'status': "success", 'message': "user login successfully"}
+        )
+
+    @staticmethod
+    def deposit(user_id, amount):
+        session = DataBaseHandeler.get_session()
+        user = session.query(User).filter_by(user_id=user_id).first()
+        # TODO: we need to decrypt the value before doing the calculation
+        user.balance = str(float(amount) + float(user.balance))
+        session.commit()
+        return json.dumps(
+            {'status': "success", 'message': f"balance update successfully {user.balance}"}
+        )
+
+    @staticmethod
+    def withdraw(user_id, amount):
+        session = DataBaseHandeler.get_session()
+        user = session.query(User).filter_by(user_id=user_id).first()
+        # TODO: we need to decrypt the value before doing the calculation
+        if (float(user.balance) >= float(amount)):
+            user.balance = str(float(user.balance) - float(amount))
+            session.commit()
+            return json.dumps(
+                {'status': "success",
+                    'message': f"balance update successfully {user.balance}"}
+            )
+
+        return json.dumps(
+            {'status': "error", 'message': f"insufficient balance {user.balance}"}
+        )
+
+    @staticmethod
+    def get_balance(user_id: int):
+        session = DataBaseHandeler.get_session()
+        user = session.query(User).filter_by(user_id=user_id).first()
+
+        if (not user):
+            return json.dumps(
+                {'status': "error", 'message': "User does not exists"}
+            )
+
+        return json.dumps(
+            {'status': "success", 'message': f"your balance is {user.balance}"}
+        )
 
 
 Base.metadata.create_all(DataBaseHandeler.get_connection())
